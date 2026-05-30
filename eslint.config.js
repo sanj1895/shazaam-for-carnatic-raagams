68049d91881b9f257a91e9ffcef7ec496e5929fe
Sanjana Anand <asanj556556@gmail.com>
2026-05-29 20:50:51 -0500

    Prepare repo for public hackathon submission

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,13 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    'node_modules',
+    '.venv',
+    'agent/.adk',
+    'agent/__pycache__',
+  ]),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
